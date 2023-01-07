1b55293d038616ead7043d323bd399cae0d658c3
Your Name <you@example.com>
2023-01-07 21:04:21 +0000

    working on

--- pages/test.jsx
+++ pages/test.jsx
@@ -7,13 +7,9 @@ import { motion, Variants, HTMLMotionProps } from "framer-motion";
 import { RotateCcw } from "react-feather";
 import { FC } from "react";
 
-const WavyText = ({
-  text,
-  delay = 0,
-  duration = 0.05,
-  replay,
-  ...props
-}) => {
+function WavyText({
+  text, delay = 0, duration = 0.05, replay, ...props
+}) {
   const letters = Array.from(text);
 
   const container = {
@@ -22,7 +18,7 @@ const WavyText = ({
     },
     visible: (i = 1) => ({
       opacity: 1,
-      transition: { staggerChildren: duration, delayChildren: i * delay,   }
+      transition: { staggerChildren: duration, delayChildren: i * delay, }
     })
   };
 
@@ -32,7 +28,7 @@ const WavyText = ({
       y: 0,
       transition: {
         repeat: "Infinity",
-        duration: 5,
+        duration: 4,
         type: "spring",
         damping: 12,
         stiffness: 200
@@ -43,7 +39,7 @@ const WavyText = ({
       y: 20,
       transition: {
         repeat: "Infinity",
-        duration: 5,
+        duration: 3,
         type: "spring",
         damping: 12,
         stiffness: 200
@@ -53,12 +49,12 @@ const WavyText = ({
 
   return (
     <motion.h1
-      style={{ 
-        display: "flex", 
+      style={{
+        display: "flex",
         overflow: "hidden",
-      color: "#fff",
-      fontSize: 90,
-    }}
+        color: "#fff",
+        fontSize: 50,
+      }}
       variants={container}
       initial="hidden"
       animate={replay ? "visible" : "hidden"}
@@ -71,44 +67,152 @@ const WavyText = ({
       ))}
     </motion.h1>
   );
-};
+}
 
 
 
-export default function App() {
+export function HeroAnima1() {
   const [replay, setReplay] = useState(true);
   return (
     <main>
       <div className="main-text">
-        <WavyText text="Awesome Wavy Text!" replay={replay} />
-        <button
-          onClick={() => {
-            setReplay(!replay);
-            setTimeout(() => setReplay(true), 600);
-          }}
-        >
-          Replay <RotateCcw />
-        </button>
+        <WavyText text="Welcome to Emso & Co" replay={replay} />
       </div>
       <style jsx>{`
     main{
-      background: linear-gradient(
-    45deg,
-    hsl(272deg 75% 65%) 0%,
-    hsl(193deg 100% 50%) 50%,
-    hsl(162deg 84% 88%) 100%
-  );
     }
     .main-text{
       font-family: "Lexend Deca", sans-serif;
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
   justify-content: center;
   align-items: center;
   user-select: none;
     }
     `}</style>
     </main>
+  );
+}
+
+
+export function HeroAnima2() {
+  const [replay, setReplay] = useState(true);
+  return (
+    <main>
+      <div className="main-text">
+        <WavyText text="We are trully Global" replay={replay} />
+      </div>
+      <style jsx>{`
+    main{
+    }
+    .main-text{
+      padding-top: 1rem;
+      font-family: "Lexend Deca", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+    }
+    `}</style>
+    </main>
+  );
+}
+
+
+
+
+function Hero() {
+  return (
+    <div>
+      <div className="w-11/12 sm:w-2/3 mb-2 sm:mb-10 pt-14">
+        <h1 className="font text-2xl  tracking-tight  text-gray-100 dark:text-gray-200 sm:text-6xl">
+        <Textra effect='press' 
+       duration={3000}
+      
+      data={[' We are helping hands whenever you are in trouble', ]} />
+        </h1>
+      </div>
+      <div className="flex justify-center items-center">
+      </div>
+    </div>
+  );
+}
+
+function Hero2() {
+  return (
+    <div>
+      <div className="w-11/12 sm:w-2/3 mb-5 sm:mb-10 pt-14">
+        <br />
+        <h1 className="font text-2xl tracking-tight text-gray-100 dark:text-gray-200 sm:text-6xl">
+        <Textra effect='rightLeft' 
+       duration={3000}
+      
+      data={['We are by your side wherever you are.', ]} />
+        </h1>
+
+      </div>
+      <div className="flex justify-center items-center">
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+export default function SwiperHero() {
+  return (
+    <div className="swiper-container">
+      <div className="swiper-main">
+        <Swiper centeredSlides={true} spaceBetween={30} effect={"fade"} loop={true} zoom={{ maxRatio: 6 }} autoplay={{ delay: 3000, disableOnInteraction: false, }} speed={6000} pagination={{ clickable: true, }} navigation={true} modules={[EffectFade, Autoplay, Pagination, Navigation, Zoom]} className="mainSwiper"
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          }}>
+          <SwiperSlide className="mbg-no-repeat bg-cover bg-center	bg-[url('../public/bg-sm.webp')] xl:bg-[url('../public/bg.webp')] h-100 max-h-full " >
+            <div className="pt-20 container h-screen ">
+              <HeroAnima1/>
+              <Hero/>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-no-repeat bg-cover bg-center	bg-[url('../public/bg-slider1-layer2.webp')] xl:bg-[url('../public/bg-slider1-layer2.webp')] h-100 max-h-full">
+            <div className="pt-80 container h-screen">
+              <HeroAnima2/>
+              <Hero2/>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <style jsx>{`
+            `}</style>
+      <style jsx global>{`
+                #app {
+                    height: 100%;
+                }
+                html,
+                body {
+                    position: relative;
+                    height: 100%;
+                }
+
+                body {
+                    background: #eee;
+                    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+                    color: #000;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                * {
+                    box-sizing: border-box;
+                }
+            `}</style>
+    </div>
   );
 }

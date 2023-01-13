d235f4178ccfb8d20dbb2a7bb4d2c750216c315b
Your Name <you@example.com>
2023-01-13 10:03:31 +0000

    removing dark

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes'
 import 'swiper/css';
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
@@ -8,11 +7,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+    return <Component {...pageProps} />
+
 }
 
 export default MyApp

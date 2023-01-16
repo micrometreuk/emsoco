48422c3e281c3047a5a255b5deaeb554da668179
Your Name <you@example.com>
2023-01-16 22:32:57 +0000

    sorting bg

---
+++ components/Features.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import { CheckCircleIcon } from '@heroicons/react/solid'
+
+
+export default function Features() {
+
+    const features = [
+        { name: "Private Security", description: "Commitment to excellence through expertise and care" },
+        { name: "Maintenance", description: "Offices in London and Paris" },
+        { name: "Optimization", description: "Pro-active, professional and forward-thinking" },
+        { name: "Integration", description: "Free assessment of your case and competitive rates" },
+    ]
+
+    return (
+        <div className="relative mx-auto py-10 px-6 w-full max-w-7xl bg-white">
+            <div className="relative w-full flex flex-col items-center text-gray-800">
+                <dl className="w-full grid grid-cols-4 gap-8">
+                    {features.map(feature => (
+                        <div className="col-span-full sm:col-span-2 lg:col-span-1 flex">
+                            <div  className="flex-shrink-0 mr-2 w-8 h-8 text-green-600" />&rarr;
+                            <div className="inline-flex flex-col space-y-3">
+                                <dd className="text-gray-600 font-semibold">{feature.description}</dd>
+                            </div>
+                        </div>
+                    ))}
+                </dl>
+            </div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,14 @@ import Hero from "../components/Hero";
 import Landing from "../components/Landing";
 import About from "../components/About";
 import PromoSection from "../components/PromoSection";
+import Features from "../components/Features";
 export default function Home() {
   return (
     <Layout>
       <Landing/>
       <Hero/>
       <About/>
+      <Features/>
       <PromoSection/>
     </Layout>
   );

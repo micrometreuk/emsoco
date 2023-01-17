8a81887075e4e0aedbaf388dad61e7ee79413226
Your Name <you@example.com>
2023-01-17 10:09:35 +0000

    adjusting components/Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,66 +1,42 @@
 import React, { useMemo } from "react";
 import { motion } from "framer-motion";
-import getScrollAnimation from "../utils/getScrollAnimation";
-import ScrollAnimationWrapper from "./ScrollAnimationWrapper";
 
 
-const Hero = ({
-  listUser = [
-    {
-      name: "This phenomenally success rate reflects the hard-work, tenacity, experience and undeniable expertise of our specialists.",
-      number: "+75% Winning guarantee ",
-      icon: "/assets/Icon/heroicons_sm-user.svg",
-    },
-    {
-      name: "Every aspect of your case is protected and hyper-secure, with levels of access and confidentiality of the highest priority.",
-      number: "Secure management",
-      icon: "/assets/Icon/gridicons_location.svg",
-    },
-    {
-      name: "Our helpline is closely monitored and we offer live chat and social media customer service 24 hours a day, 7 days a week, 365 days a year.",
-      number: "Full time support",
-      icon: "/assets/Icon/bx_bxs-server.svg",
-    },
-  ],
-}) => {
-  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-
+export default function Landing() {
   return (
-    <div className="mt-0 pt-0  relative w-full h-full ">
-      <ScrollAnimationWrapper>
-        <motion.div variants={scrollAnimation}>
-      <div className="relative w-full flex">
-        <div className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-3 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500  z-10">
-          {listUser.map((listUsers, index) => (
-            <div
-              className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-2 sm:w-auto mx-auto sm:mx-0"
-              key={index}
-            >
-              <div 
-              className="flex "
-              >
-                <div 
-                className="flex flex-col"
-                >
-                  <p 
-                  className="text-xl text-black-600 font-bold"
-                  >
-                    {listUsers.number}
-                  </p>
-                  <p className="text-lg text-black-500">{listUsers.name}</p>
-                </div>
+    <>
+      <main>
+        <section className="pb-2 relative block bg-gray-900">
+          <div className="container mx-auto px-4 lg:pt-2 lg:pb-4">
+            <div className="flex flex-wrap mt-8 justify-center">
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <h6 className="text-xl mt-5 font-semibold text-white">
+                    +75% Winning guarantee
+                </h6>
+                <p className="mt-2 mb-4 text-gray-500">
+                    This phenomenally success rate reflects the hard-work, tenacity, experience and undeniable expertise of our specialists.
+                </p>
+              </div>
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <h5 className="text-xl mt-5 font-semibold text-white">
+                      Secure management
+                </h5>
+                <p className="mt-2 mb-4 text-gray-500">
+                      Every aspect of your case is protected and hyper-secure, with levels of access and confidentiality of the highest priority.
+                </p>
+              </div>
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <h5 className="text-xl mt-5 font-semibold text-white">
+                    Full time support
+                </h5>
+                <p className="mt-2 mb-4 text-gray-500">
+                      Our helpline is closely monitored and we offer live chat and social media customer service 24 hours a day, 7 days a week, 365 days a year.
+                </p>
               </div>
             </div>
-          ))}
-        </div >
-      </div>
-        </motion.div>
-      </ScrollAnimationWrapper>
-    </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
-};
-
-
-
-
-export default Hero;
+}

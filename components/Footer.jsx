da86a40eea383ab5d4acd56386d289d4da0ce6ae
Your Name <you@example.com>
2023-01-19 09:31:57 +0000

    adding fa

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import CopyRight from './Copyright'
+import { FaPhone, FaWhatsapp, FaEnvelope, FaHome} from "react-icons/fa";
 
 export function Footer() {
   return (
@@ -48,29 +49,52 @@ export default function Footer3() {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap">
           <div className="w-full lg:w-6/12 px-4">
-          <span className=" bg-gray-200" aria-hidden="true" />
-                                <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
-                                    <Image
-                                        className=" focus:ring-indigo-500 h-8 w-auto"
-                                        src="/logo-bg.webp"
-                                        alt="logo"
-                                        width={70}
-                                        height={48}
-                                        priority
-                                    />
-                                    <span className="ml-6 italic block text-sm font-medium">
-                                        Emso and Co
-                                        Solicitors
-                                    </span>
-                                </a>
+            <span className=" bg-gray-200" aria-hidden="true" />
+            <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
+              <Image
+                className=" focus:ring-indigo-500 h-8 w-auto"
+                src="/logo-bg.webp"
+                alt="logo"
+                width={70}
+                height={48}
+                priority
+              />
+              <span className="ml-6 italic block text-sm font-medium">
+                Emso and Co
+                Solicitors
+              </span>
+            </a>
             <h5 className="text-lg mt-0 mb-2 text-gray-700">
-            We are always here for you, 7 days a week. Contact us and we will take it from here.
+              We are always here for you, 7 days a week. Contact us and we will take it from here.
             </h5>
-            +44 (0)79 5186 7712
-            <br />
-enquiries@emsoandcosolicitors.com
-            <br />
-PO Box 79106, London, SE26 9FD
+
+            <div className="mt-6 cursor-pointer flex items-center space-x-10 ">
+              <p className="flex flex-col">
+                <FaPhone />
+                <Link href="tel:07951867712">
+                  <span className="text-lg text-black font-medium"> +44 (0)79 5186 7712</span>
+                </Link>
+              </p>
+            </div>
+            <div className="mt-6 cursor-pointer flex items-center space-x-10 ">
+              <p className="flex flex-col">
+                <FaEnvelope />
+                <Link href="mailto:enquiries@emsoandcosolicitors.com">
+                  <span className="text-lg text-black font-medium">
+            enquiries@emsoandcosolicitors.com
+                  </span>
+                </Link>
+              </p>
+            </div>
+
+            <div className="mt-6 cursor-pointer flex items-center space-x-10 ">
+              <p className="flex flex-col">
+                <FaHome />
+                  <span className="text-lg text-black font-medium">
+            PO Box 79106, London, SE26 9FD
+                  </span>
+              </p>
+            </div>
           </div>
           <div className="w-full lg:w-6/12 px-4">
             <div className="flex flex-wrap items-top mb-6">
@@ -81,54 +105,54 @@ PO Box 79106, London, SE26 9FD
                 <ul className="list-unstyled">
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="">Contact
+                      href="">Contact
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="">Faqs
+                      href="">Faqs
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="">Appointment
+                      href="">Appointment
                     </a>
                   </li>
                 </ul>
               </div>
               <div className="w-full lg:w-4/12 px-4">
                 <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
-                POLICIES
+                  POLICIES
                 </span>
                 <ul className="list-unstyled">
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                    href="/">Code of Practice
-                  </a>
-                  </li>
-                  <li>
-                    <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="/">Complaint Policy
+                      href="/">Code of Practice
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="/">Diversity Policy
+                      href="/">Complaint Policy
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="/">Pricing Policy
+                      href="/">Diversity Policy
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="/privacy">Privacy Policy
+                      href="/">Pricing Policy
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="/">Fees
+                      href="/privacy">Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href="/">Fees
                     </a>
                   </li>
                 </ul>
--- components/PromoSection.jsx
+++ components/PromoSection.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link';
+import { FaPhone, FaWhatsapp } from "react-icons/fa";
 
-export default  function PromoSection() {
+export default function PromoSection() {
   return (
     <div className="relative mx-auto px-4 w-full max-w-7xl bg-white">
       <div className="grid grid-cols-1 md:grid-cols-2">
@@ -13,28 +15,31 @@ export default  function PromoSection() {
             </p>
           </div>
           {/* ::Details */}
-          <div className="mt-6 flex items-center space-x-10">
+          <div className="mt-6 cursor-pointer flex items-center space-x-10 ">
             {/* :::price */}
             <p className="flex flex-col">
               <span className="text-xs text-gray-400 font-semibold">Book yours on</span>
-              <span className="text-3xl text-black font-medium"> +44 (0)79 5186 7712</span>
+              <FaPhone />
+              <Link href="tel:07951867712">
+                <span className="text-3xl text-black font-medium"> +44 (0)79 5186 7712</span>
+              </Link>
             </p>
           </div>
         </div>
         {/* :PICTURE CONTAINER */}
 
         <div className="order-first md:order-last col-span-1 md:pl-6 flex justify-center items-center">
-        <Image
-          priority="quality"
-          src="/signature.webp"
-          alt="image slide1"
-          width={80}
-          height={80}
-        />
-            <p className="flex flex-col">
-              <span className="text-3xl text-black font-medium">Emile Kouame</span>
-              <span className="text-xs text-gray-400 font-semibold">Principal Solicitor</span>
-            </p>
+          <Image
+            priority="quality"
+            src="/signature.webp"
+            alt="image slide1"
+            width={80}
+            height={80}
+          />
+          <p className="flex flex-col">
+            <span className="text-3xl text-black font-medium">Emile Kouame</span>
+            <span className="text-xs text-gray-400 font-semibold">Principal Solicitor</span>
+          </p>
         </div>
       </div>
     </div>
